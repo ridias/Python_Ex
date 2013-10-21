1c6c170d59bef5362d2ed179800a67ddf9869f41
ridias <ricardodias_5@hotmail.com>
2013-10-21 19:01:35 +0200

    Proyecto Horario 2
    Implementación de reloj, reloj actual, verificación y otras cosas

--- Proyectos/ControlHorario.py
+++ Proyectos/ControlHorario.py
@@ -1,6 +1,10 @@
+import time
+import os
+
 print "Bienvenido a mi programa"
 print "Este programa consistira en un control de horario"
 print "Dispondremos de la informacion basica de los empleados y cuando pueden estar en un lugar y en otro"
+
 
 
 def guardarEnLista(elemento, *algomas):
@@ -13,18 +17,67 @@ def guardarEnLista(elemento, *algomas):
 	return lista
 	
 	
+def verificar(lista):
+	comptador = 0
+	for elemento in lista:
+		if elemento == False or (elemento == "Gym" or elemento == "gym"):
+			comptador = comptador + 1
+			if comptador == 2:
+				print "No puedes estar en el gym, has depasado la hora"
+			else:
+				print "Puedes acceder a la sala Gym, tienes 1 hora para hacer ejercicio a partir de ahora"
+			
+def relojActual(cambiar):
+	if cambiar == 59:
+		cambiar = 0
+	
+	return cambiar
 
+			
+def relojEmpresarial(horActual, minutosActual, segundosActual, limite, lugar):
+
+	comptadorSegundos = 0
+	comptadorMinutos = 0
+	comptadorHoras = 0
+	while limite != 0:
+		if comptadorSegundos != 60:
+			time.sleep(1)
+			segundosActual = segundosActual + 1
+			segundosActual = relojActual(segundosActual)
+			comptadorSegundos = comptadorSegundos + 1
+			print "Hora actual: ", horActual, ":", minutosActual, ":", segundosActual
+			print "Tiempo que te queda: ", comptadorHoras, ":", comptadorMinutos, ":", comptadorSegundos
+			if comptadorSegundos == 59:
+				comptadorSegundos = 0
+				comptadorMinutos = comptadorMinutos + 1
+				minutosActual = minutosActual + 1
+				os.system('cls')				#equivalente a clrscr() de pascal o c++ importando arriba
+				if comptadorMinutos == 59:
+					comptadorMinutos = 0
+					comptadorHoras = comptadorHoras + 1
+					horActual = horActual + 1
+					limite = limite - 1
+					
+	if limite == 0 and lugar == "Gym":
+		print "Ya has terminado tu hora de ejercicio en el gymnasio"
+	elif limite == 0 and lugar == "Trabajo":
+		print "Ya has acabado de trabajar, te puedes ir a casa"
+	
+	
+	
 class Empleado(object):
 	
 	#atributos principales
-	def __init__(self, id, nombre, apellido, edad, oficio, departamento):
+	def __init__(self, id, nombre, apellido, edad, oficio, departamento, sala):
 		self.id = id
 		self.nombre = nombre
 		self.apellido = apellido
 		self.edad = edad
 		self.oficio = oficio
 		self.departamento = departamento
-		lista = guardarEnLista(id, nombre, apellido, edad, oficio, departamento)
+		self.sala = sala
+		permitirAcceso = False
+		lista = guardarEnLista(id, nombre, apellido, edad, oficio, departamento, sala, permitirAcceso)
 		
 	
 	#obtener una informacion especifica del empleado
@@ -50,25 +103,29 @@ class Empleado(object):
 		elif elemento == "departamento" or elemento == "Departamento":
 			return self.departamento
 			print "Has recibido perfectamente la informacion"
+		elif elemento == "sala" or elemento == "sala":
+			return self.sala
+			print "Has recibido perfectamente la informacion"
 			
 	def getTodoEmpleado(self):
 		print lista
 	
 	
 	#Funcion de modificar la totalidad de la informacion de un Empleado
-	def setTodoEmpleado(self, id, nombre, apellido, edad, oficio, departamento):
+	def setTodoEmpleado(self, id, nombre, apellido, edad, oficio, departamento, sala):
 		self.id = id
 		self.nombre = nombre
 		self.apellido = apellido
 		self.edad = edad
 		self.oficio = oficio
 		self.departamento = departamento
+		permitirAcceso = False
 		
-		informacion = guardarEnLista(id, nombre, apellido, edad, oficio, departamento)
+		informacion = guardarEnLista(id, nombre, apellido, edad, oficio, departamento, sala, permitirAcceso)
 		print "La informacion se ha cambiado perfectamente"
 		print informacion
 		
-	def setEmpleado(self, elemento)
+	def setEmpleado(self, elemento):
 		print "La unica informacion que puedes cambiar son (escoge entre uno de estos): id, nombre, apellido, edad, oficio, departamento."
 		elemento = str(raw_input("Que informacion quieres cambiar sobre algun empleado: "))
 		
@@ -96,14 +153,14 @@ class Empleado(object):
 			self.departamento = elemento
 			lista.insert(5, elemento)
 			print "La informacion se ha cambiado perfectamente"
-			
-			
-		
 	
 
 #Pruebas	
-carlos = Empleado(1, "Carlos", "Dias", 45, "Programador", "Java")
-carlos.setTodoEmpleado(2, "Carlos", "Dias", 42, "Programador", "Java")
+#carlos = Empleado(1, "Carlos", "Dias", 45, "Programador", "Java")
+#carlos.setTodoEmpleado(2, "Carlos", "Dias", 42, "Programador", "Java")
+
+relojEmpresarial(18,9,0,1,"Gym")
+os.system('cls')				#equivalente a clrscr() de pascal o c++ importando arriba
 
 		
 		
