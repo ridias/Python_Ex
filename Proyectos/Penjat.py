a2e183fdc7afda2ba6a6dd31dfa3384e40d09411
ridias <ricardodias_5@hotmail.com>
2013-10-22 11:19:35 +0200

    Penjat
    Juego del colgado

---
+++ Proyectos/Penjat.py
@@ -0,0 +1,45 @@
+print "Benvingut al meu programa"
+print "Aquest programa sera una simulacio del penjat"
+
+cadena = str(raw_input("Entra la paraula que vols que la persona adivini: ")
+nombrErrors = 0  #total d'errors = 12
+
+def separar(cadena):
+	lista[]
+	for caracter in cadena:
+		lista.append(caracter)
+		print lista
+		
+		return lista
+		
+
+def buscar(lista, lista2, lletra):
+	pos = 0
+	trobat = False
+	limit = len(lista)
+	while limit != 0:
+		if caracter == lletra:
+			inserir(lista2, pos, caracter)
+		limit = limit - 1
+		pos = pos + 1
+			
+#vigila casos extrems		
+
+def inserir(lista, pos, caracter):
+	lista.insert(pos, caracter)
+	
+	return lista
+	
+def verificar(lista):
+
+	return #true o false
+	
+	
+letra = str(raw_input("Entra la primera lletra: "))
+guanyat = False
+
+while nombrErrors != 12 and guanyat == False:
+	
+	letra = str(raw_input("Entra la seguent lletra: "))
+	
+	
