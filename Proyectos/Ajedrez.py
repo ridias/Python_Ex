13351c9877e698bb3eaa5983165001da81675a68
ridias <ricardodias_5@hotmail.com>
2013-11-06 05:50:07 +0100

    Ajedrez Modulo
    Definir las piezas como clases y el tablero, hacemos diseño descendent
    para saber mas o menos que hacer

---
+++ Proyectos/Ajedrez.py
@@ -0,0 +1,23 @@
+class tablero(object):
+	
+	def __init__(self):
+		
+
+
+class peon(object):
+
+	def __init__(self):
+		id = self.id
+		idColor = self.idColor
+		puntos = self.puntos
+		posX = self.posX
+		posY = self.posY
+		
+	def dosPasos(self):
+		#funcion que permite avanzar dos casillas hacia delante
+		
+	def comerPieza(self):
+		#funcion que permite saber si puede comer una pieza o no, una verificacion!
+		
+	def unPaso(self):
+		#funcion que permite avanzar una casilla hacia delante
